48ac2871a5f66211390387768ce4d96b392eede7
Your Name <you@example.com>
2025-09-09 13:41:30 +0200

    concentrate decision code

--- decide.py
+++ decide.py
@@ -1,8 +1,31 @@
 import inference
 
-def decide(theory, world_model, observations, utility_node, physical_identity, logical_identity):
-    # perform surgery on the factor graph
-    modified_model, intervention_node, _ = theory(world_model, observations, utility_node, physical_identity, logical_identity)
+def decide(theory, world_model, observations, utility_node, physical_identity, logical_identity, args=None):
+    if args and args.initial_verbose:
+        world_model.view(filename="Initial")
+    if args and args.verbose:
+        print(f"INITIAL NODES:")
+        for node, values in world_model.nodes.items():
+            print(f"  {node:20s} ∊ {values}")
+        print(f"INITIAL FACTORS:")
+        for factor in world_model.factors:
+            print(f"  {factor.consequence:20s} <= {factor.causes}")
+    
+    # use the decision theory to perform surgery to get a factor graph where our
+    # decision can be taken by maximizing expected utility coniditioned on a single
+    # "intervention" node. In the below, this intervention node is the name of a node
+    # in modified_model
+    modified_model, intervention_node, output_formatter = theory(world_model, observations, utility_node, physical_identity, logical_identity)
+
+    if args and args.modified_verbose:
+        modified_model.view(filename="Modified")
+    if args and args.verbose:
+        print(f"MODIFIED NODES:")
+        for node, values in modified_model.nodes.items():
+            print(f"  {node:20s} ∊ {values}")
+        print(f"MODIFIED FACTORS:")
+        for factor in modified_model.factors:
+            print(f"  {factor.consequence:20s} <= {factor.causes}")
 
     # go through each possible value of the intervention node and compute an expected utility
     expected_utilities = {}
@@ -11,4 +34,5 @@ def decide(theory, world_model, observations, utility_node, physical_identity, l
         expected_utilities[intervention] = expectation
 
     # pick the intervention with highest expected utility
-    return max(expected_utilities.keys(), key=lambda node_name: expected_utilities[node_name])
+    output = max(expected_utilities.keys(), key=lambda node_name: expected_utilities[node_name])
+    return output, output_formatter(output)
